96efa0ca8be3c1b2ce168da46f23581956a89f6e
develop-jaz <107145474+develop-jaz@users.noreply.github.com>
2024-04-30 16:46:08 -0500

    Update script-fp.js

--- script-fp.js
+++ script-fp.js
@@ -12,23 +12,65 @@ function addBook() {
 }
 
 // Function to delete a book from the library
-function deleteBook() {
-    var title = prompt('Enter the title of the book to delete:');
+function deleteBook(id) {
+    //var id = prompt('Enter the id of the book to delete:');
 
     // Construct SQL DELETE statement
-    var sql = "DELETE FROM books WHERE title='" + title + "'";
+    var sql = "DELETE FROM books WHERE id='" + id + "'";
 
     // Send SQL statement to server for execution
     executeSqlStatement(sql);
 }
+// Function to delete a book
+function deleteBook(id) {
+    if (confirm("Are you sure you want to delete this book?")) {
+        // Send AJAX request to delete the book
+        $.ajax({
+            type: "POST",
+            url: "main.php",
+            data: {
+                delete_book: true,
+                id: id
+            },
+            success: function(response) {
+                alert(response);
+                // Reload the page after deletion
+                location.reload();
+            }
+        });
+    }
+}
+
+// Function to change the rating of a book
+function changeRating(id) {
+    var newRating = prompt("Enter the new rating:");
+    if (newRating !== null && newRating !== "") {
+        // Send AJAX request to change the rating of the book
+        $.ajax({
+            type: "POST",
+            url: "main.php",
+            data: {
+                change_rating: true,
+                id: id,
+                new_rating: newRating
+            },
+            success: function(response) {
+                alert(response);
+                // Reload the page after rating change
+                location.reload();
+            }
+        });
+    }
+}
+
 
 // Function to change the rating of a book in the library
-function changeRating() {
-    var title = prompt('Enter the title of the book to change rating:');
+/*function changeRating(id) {
+    //var id = prompt('Enter the title of the book to change rating:');
     var newRating = prompt('Enter the new rating for the book:');
 
     // Construct SQL UPDATE statement
-    var sql = "UPDATE books SET rating='" + newRating + "' WHERE title='" + title + "'";
+    var sql = "UPDATE books SET rating='" + newRating + "' WHERE id'" + id + "'";
 
     // Send SQL statement to server for execution
     executeSqlStatement(sql);
@@ -52,4 +94,4 @@ function executeSqlStatement(sql) {
         }
     };
     xhr.send('sql=' + encodeURIComponent(sql));
-}
+}*/
